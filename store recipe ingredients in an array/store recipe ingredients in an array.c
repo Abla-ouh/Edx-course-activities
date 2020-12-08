c2a9dff396341e17ada71e239fcfb580ac02d2e5
Abla-ouh <57642906+Abla-ouh@users.noreply.github.com>
2020-12-09 00:36:53 +0100

    Create store recipe ingredients in an array.c

---
+++ store recipe ingredients in an array/store recipe ingredients in an array.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+int main(){
+    int arr[10];
+    int index;
+    int i;
+    for(i=0 ; i<10 ; i++){
+        scanf("%d",&arr[i]);}
+        
+    scanf("%d",&index);
+    if(index == 0){
+        printf("%d",arr[0]);
+    }
+    else if(index == 1){
+        printf("%d",arr[1]);
+    }
+      else if(index == 2){
+        printf("%d",arr[2]);
+    }
+      else if(index == 3){
+        printf("%d",arr[3]);
+    }
+      else if(index == 4){
+        printf("%d",arr[4]);
+    }
+      else if(index == 5){
+        printf("%d",arr[5]);
+    }
+      else if(index == 6){
+        printf("%d",arr[6]);
+    }
+      else if(index == 7){
+        printf("%d",arr[7]);
+    }
+      else if(index == 8){
+        printf("%d",arr[8]);
+    }
+      else if(index == 9){
+        printf("%d",arr[9]);
+    }
+   
+}
